2809edb7c644fe38042566b55cf4a8c44aa1fe2f
Swojeet K <swojeetk@swojeet>
2016-08-20 13:29:02 +0545

    Writing Post Backend

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
+
 	def new
 		@post = Post.new
-		
 	end
+
+	def create
+		@post = current_user.posts.create(post_params)
+		if @post.valid?
+			redirect_to root_path
+		else
+			render :new, status: :inprocessable_entity
+		end
+	end
+
+	private
+
+	def post_params
+		params.require(:post).permit(:user_id, :photo, :description)
+	end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
+	
+	belongs_to :user
+
 	mount_uploader :photo, PhotoUploader
+
+	validates :photo, :description, :user_id, presence: true
 end
