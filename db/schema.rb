a89342abcd44d22965f936fd6c9a1aabdaeaf430
Swojeet K <swojeetk@swojeet>
2016-08-20 17:52:49 +0545

    Controllers and models for comments created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160820044105) do
+ActiveRecord::Schema.define(version: 20160820115030) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "post_id"
+    t.integer  "user_id"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
+  add_index "comments", ["user_id", "post_id"], name: "index_comments_on_user_id_and_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "photo"
