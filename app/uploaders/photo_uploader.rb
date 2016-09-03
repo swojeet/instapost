fb5e9df67b5812c480d352b621aeb149d234afd4
swojeet <swojeetkayastha@gmail.com>
2016-09-03 12:48:31 +0545

    some fixes

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -8,22 +8,10 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   include Cloudinary::CarrierWave
 
-  process :convert => 'png'
-  process :tags => ['post_photo']
-
-  version :standard do
-    process :resize_to_fill => [100, 150, :north]
-  end
-
-  version :thumbnail do
-    eager
-    resize_to_fit(50, 50)
-  end
-
   # Choose what kind of storage to use for this uploader:
-   storage :file
+  #storage :file
   # storage :fog
-
+  
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
